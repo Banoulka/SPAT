4678e8ca6e9499ae0cf6a35d79fd6b8777a9dcdb
Nathan Bamford <nathanbammy@hotmail.co.uk>
2020-01-08 15:29:11 +0000

    login working

--- Models/lib/Authentication.php
+++ Models/lib/Authentication.php
@@ -6,6 +6,42 @@ class Authentication {
 
     private static $sessionID = "user";
 
+    public static function logonUser(User $user)
+    {
+        Session::setSession(Authentication::$sessionID, serialize($user));
+    }
+
+    /**
+     * @return User
+     */
+    public static function User()
+    {
+        if (!Authentication::isLoggedIn()) {
+            return null;
+        }
+        return unserialize(Session::getSession(Authentication::$sessionID));
+    }
+
+    public static function validateAndLoginUser($username, $password)
+    {
+        $errors = [];
+
+        $user = User::findByUsername($username);
+
+        if ($user) {
+            if (password_verify($password, $user->password())) {
+                // Login Successfull
+                Authentication::logonUser($user);
+            } else {
+                array_push($errors, "Username or password does not match");
+            }
+        }
+        else {
+            array_push($errors, "Username does not exist");
+        }
+        return $errors;
+    }
+
     public static function isRole($roleName)
     {
         // Get role name,
@@ -16,6 +52,6 @@ class Authentication {
 
     public static function isLoggedIn()
     {
-        return true;
+        return Session::isSet(Authentication::$sessionID);
     }
 }
