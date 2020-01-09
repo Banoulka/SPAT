a6a78978897c5a593372b52148424c9ddba53b6a
calum young <calumyoung99@live.co.uk>
2020-01-09 13:11:52 +0000

    something

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 session_start();
 spl_autoload_register(function ($className) {
-   require_once "/Models/lib/$className.php";
+   require_once "Models/lib/$className.php";
 });
 
 
--- login.php
+++ login.php
@@ -1,3 +1,6 @@
 <?php
-
+session_start();
+spl_autoload_register(function ($className) {
+    require_once "Models/lib/$className.php";
+});
 require_once "Views/login.phtml";
