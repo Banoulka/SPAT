41627d4fd00caff1242d33eb34e2740bcde7e2c5
Harry Robson <harry.robson1@gmail.com>
2020-01-09 13:11:55 +0000

    admin dboard btns

--- adminDashboard.php
+++ adminDashboard.php
@@ -1,1 +1,13 @@
 <?php
+
+// Bootstrap
+session_start();
+spl_autoload_register(function ($className) {
+    require_once "Models/lib/$className.php";
+});
+
+
+
+include_once "Views/adminDashboard.phtml";
+
+
