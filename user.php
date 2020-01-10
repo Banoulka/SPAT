232814eee95b721f769c2fbd2b91b2f975e62ba3
Harry Robson <harry.robson1@gmail.com>
2020-01-10 15:29:32 +0000

    require changes

--- user.php
+++ user.php
@@ -5,12 +5,7 @@ spl_autoload_register(function ($className) {
     require_once "Models/lib/$className.php";
 });
 
-require_once "Views/Template/header.phtml";
-
-
 
 require_once "Views/user.phtml";
-
-require_once "Views/Template/footer.phtml";
 
 
