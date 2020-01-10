cb0b19e675f91a85db1a149927b8569819cdec42
Harry Robson <harry.robson1@gmail.com>
2020-01-10 15:06:13 +0000

    user page

---
+++ user.php
@@ -0,0 +1,16 @@
+<?php
+// Bootstrap
+session_start();
+spl_autoload_register(function ($className) {
+    require_once "Models/lib/$className.php";
+});
+
+require_once "Views/Template/header.phtml";
+
+
+
+require_once "Views/user.phtml";
+
+require_once "Views/Template/footer.phtml";
+
+
