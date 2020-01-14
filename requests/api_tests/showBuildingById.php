6af364aaaee63acc4c106baf64f9df6795325a1e
Nathan Bamford <nathanbammy@hotmail.co.uk>
2020-01-14 11:36:16 +0000

    start of auth demo - building

--- requests/api_tests/showBuildingById.php
+++ requests/api_tests/showBuildingById.php
@@ -7,12 +7,12 @@ spl_autoload_register(function ($className) {
     require_once "../../Models/lib/$className.php";
 });
 
-// TODO: Do auth checks
-$data = API::getBuildingByID(3);
-echo json_encode($data);
-
-
-//$data = new stdClass();
-//$data->error = "You do not have authorisation for this";
-//echo json_encode($data);
+if (Authorisation::hasAuth("get")) {
+    $data = API::getBuildingByID("5e18935e1fb6ab001271a82a");
+    echo json_encode($data);
+} else {
+    $data = new stdClass();
+    $data->error = "You do not have authorisation for this";
+    echo json_encode($data);
+}
 
--- requests/api_tests/showDemographicsById.php
+++ requests/api_tests/showDemographicsById.php
@@ -7,12 +7,12 @@ spl_autoload_register(function ($className) {
     require_once "../../Models/lib/$className.php";
 });
 
-// TODO: Do auth checks
-$data = API::getDemographicsById(7);
-echo json_encode($data);
-
-
-//$data = new stdClass();
-//$data->error = "You do not have authorisation for this";
-//echo json_encode($data);
+if (Authorisation::hasAuth("get")) {
+    $data = API::getDemographicsById("5e1604791c7a63001279fb0e");
+    echo json_encode($data);
+} else {
+    $data = new stdClass();
+    $data->error = "You do not have authorisation for this";
+    echo json_encode($data);
+}
 
