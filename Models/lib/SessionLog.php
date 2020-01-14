8708a4d6f498919aa5ab2252d0372d161a5e5f81
Nathan Bamford <nathanbammy@hotmail.co.uk>
2020-01-14 14:40:16 +0000

    session log get

--- Models/lib/SessionLog.php
+++ Models/lib/SessionLog.php
@@ -1,13 +1,35 @@
 <?php
 
-
+/**
+ * Class SessionLog
+ * @property string endpoint
+ * @property boolean succeeded
+ * @property int id
+ * @property string timestamp
+ * @property User user
+ */
 class SessionLog
 {
+    public function __construct()
+    {
+        $this->user = User::findByID($this->user_id);
+        unset($this->user_id);
+    }
+
     public static function createLog($dataArr)
     {
         $dataArr["user_id"] = Authentication::User()->id();
         QueryBuilder::getInstance()
             ->table("SessionLog")
             ->insert($dataArr);
+    }
+
+    public static function getAllLogs()
+    {
+        return QueryBuilder::getInstance()
+            ->table("SessionLog")
+            ->fetchAs("SessionLog")
+            ->orderby("timestamp")
+            ->getAll();
     }
 }
