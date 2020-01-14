4f73f8e06ffff57fd13d30dbeeae87cd1c128be3
Harry Robson <harry.robson1@gmail.com>
2020-01-14 12:02:49 +0000

    ;wuigfA

--- requests/api_tests/showDemographicsData.php
+++ requests/api_tests/showDemographicsData.php
@@ -3,11 +3,11 @@
 
 // Bootstrap
 header("Content-Type: application/json");
+require_once "../../Models/Role.php";
 session_start();
 spl_autoload_register(function ($className) {
     require_once "../../Models/lib/$className.php";
 });
-
 if (Authorisation::hasAuth("get")) {
     $data = API::getDemographicsById("5e1604791c7a63001279fb0e");
     echo json_encode($data);
--- requests/api_tests/showInfrastructureData.php
+++ requests/api_tests/showInfrastructureData.php
@@ -3,15 +3,18 @@
 
 // Bootstrap
 header("Content-Type: application/json");
+require_once "../../Models/Role.php";
 session_start();
 spl_autoload_register(function ($className) {
     require_once "../../Models/lib/$className.php";
 });
 
-// TODO: Do auth checks
-$data = API::getAllInfrastructure();
-echo json_encode($data);
+if (Authorisation::hasAuth("get")) {
+    $data = API::getAllInfrastructure();
+    echo json_encode($data);
+} else {
+    $data = new stdClass();
+    $data->error = "You do not have authorisation for this";
+    echo json_encode($data);
+}
 
-//$data = new stdClass();
-//$data->error = "You do not have authorisation for this";
-//echo json_encode($data);
